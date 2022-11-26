ca67cd8bea40e3bbe6c429d4dbf57462f9c6e0b0
Vasileios Papastergios <papastva@csd.auth.gr>
2022-11-27 00:33:42 +0200

    Added template code files for TCP/UDP client-server demos. Need to fix the package names to make them work properly.

---
+++ src/main/java/Templates/1 Race Condition/3 Sync2.java
@@ -0,0 +1,49 @@
+package race_condition;
+import java.util.ArrayList;
+
+public class Sync2 {
+    private static int NOF_THREADS = 2000;
+    private int cnt = 0;
+    private void increaseCount() {
+        // this simulates work that may be happening 
+        // before the critical section
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // only block the critical section
+        synchronized(this) {
+            this.cnt++;
+        }
+    }
+
+    protected int getCount() {
+        return this.cnt;
+    }
+
+    public static void main(String [] args) throws InterruptedException {
+        ArrayList<Thread> threads = new ArrayList<>();
+        // generate a unique instance
+        Sync2 s = new Sync2(); 
+        // generate threads
+        for (int i = 0; i < NOF_THREADS; i++) {
+            // each thread simply executes the increaseCount function
+            Thread incrThread = new Thread(new Runnable() {			
+                @Override
+                public void run() {
+                    s.increaseCount();
+                }
+            });
+            // start the thread and add it into the thread list
+            incrThread.start();
+            threads.add(incrThread);
+        }
+        // wait for all of the threads to finish
+        while (!threads.isEmpty()) {
+            threads.get(0).join();
+            threads.remove(0);
+        }
+        System.out.println("CNT:" + Integer.toString(s.getCount()) + "/" + Integer.toString(NOF_THREADS));
+    }
+}
