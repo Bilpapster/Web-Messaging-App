f18ed9567f2f5bfcd301168859e9d76fcf43e2ad
Vasileios Papastergios <papastva@csd.auth.gr>
2022-12-19 23:57:06 +0200

    Modified the Server and Client main methods in order to pass the port number as command line argument.

--- src/main/java/com/MessagingClient.java
+++ src/main/java/com/MessagingClient.java
@@ -8,14 +8,22 @@ public class MessagingClient {
     private Account userAccount;
 
     public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("No port provided through command line arguments.");
+            System.out.println("Client terminated.");
+            return;
+        }
+
         try {
-            Registry rmiRegistry = LocateRegistry.getRegistry(5000);
+            int port = Integer.parseInt(args[0]);
+            Registry rmiRegistry = LocateRegistry.getRegistry(port);
             CommunicationProtocol stub = (CommunicationProtocol) rmiRegistry.lookup("processor");
             System.out.println(stub.processRequest(args));
+        } catch (NumberFormatException e) {
+            System.out.println("\"" + args[0] + "\"" + " is not an integer! I expected an integer number as a port.");
+            System.out.println("Client terminated.");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-// papster(126), papastva(252), maria(136), nikos(271)
-
 }
--- src/main/java/com/Server.java
+++ src/main/java/com/Server.java
@@ -10,11 +10,21 @@ public class Server {
     }
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("No port provided through command line arguments.");
+            System.out.println("Server terminated.");
+            return;
+        }
+
         try {
+            int port = Integer.parseInt(args[0]);
             RequestProcessor requestProcessor = new RequestProcessor(new MessagingServer());
-            Registry rmiRegistry = LocateRegistry.createRegistry(5000);
+            Registry rmiRegistry = LocateRegistry.createRegistry(port);
             rmiRegistry.rebind("processor", requestProcessor);
             System.out.println("Server ready");
+        } catch (NumberFormatException e) {
+            System.out.println("\"" + args[0] + "\"" + " is not an integer! I expected an integer number as a port.");
+            System.out.println("Server terminated.");
         } catch (Exception e) {
             e.printStackTrace();
         }
